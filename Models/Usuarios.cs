5251feaf73aa054b64fd678438cde415e8ff1e12
ViccariGui <guipolicarpoviccari@gmail.com>
2025-03-20 15:37:27 -0300

    Classe do usuario

--- Models/Usuarios.cs
+++ Models/Usuarios.cs
@@ -1,6 +1,54 @@
-﻿namespace WorkBariri.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WorkBariri.Models
 {
     public class Usuarios
     {
+        [Column("Id")]
+        [Display(Name = "Id do Usuário")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column("AreaEsp")]
+        [Display(Name = "Area de Especialização")]
+        public string AreaEsp { get; set; } = string.Empty;
+
+        [Column("Sexo")]
+        [Display(Name = "Sexo")]
+        public string Sexo { get; set; } = string.Empty;
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("Senha")]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; } = string.Empty;
+
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public string Telefone { get; set; } = string.Empty;
+
+        [Column("CPF")]
+        [Display(Name = "CPF")]
+        public string CPF { get; set; } = string.Empty;
+
+        [Column("CEP")]
+        [Display(Name = "CEP")]
+        public string CEP { get; set; } = string.Empty;
+
+        [Column("FotoPerfil")]
+        [Display(Name = "Foto de Perfil")]
+        public string FotoPerfil { get; set; } = string.Empty;
+
+        [Column("TipoUsuario")]
+        [Display(Name = "Tipo de Usuário")]
+        public string TipoUsuario { get; set; } = string.Empty;
+
     }
 }
